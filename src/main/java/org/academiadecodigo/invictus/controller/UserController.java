f83e1427e78c9149b43ab4b1acdef80327cc3ca7
Joao Lopes <joaorfclopes@gmail.com>
2018-12-14 00:03:47 +0000

    Add form, missing validation

--- src/main/java/org/academiadecodigo/invictus/controller/UserController.java
+++ src/main/java/org/academiadecodigo/invictus/controller/UserController.java
@@ -9,10 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import javax.jws.WebParam;
-import javax.validation.Valid;
 
 @Controller
 public class UserController {
@@ -51,7 +47,6 @@ public class UserController {
         model.addAttribute("user", usertoDto.convert(user));
 
         return "user";
-
     }
 
 
@@ -65,7 +60,7 @@ public class UserController {
     }
 
     @GetMapping(path = "/user/form")
-    public String form (Model model){
+    public String add (Model model){
         model.addAttribute("user",new UserDto());
 
         return "form";
@@ -85,9 +80,5 @@ public class UserController {
 
         return "userList";
     }
-
-
-
-
 
 }
